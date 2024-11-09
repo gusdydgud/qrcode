1883faea431ce1ec63d82c6f55823a073299b137
FORrri <a01056012435@gmail.com>
2024-11-10 01:06:02 +0900

    1109 reupdate

--- liststart/src/main/java/com/example/liststart/datasource/DispatchDataSource.kt
+++ liststart/src/main/java/com/example/liststart/datasource/DispatchDataSource.kt
@@ -1,18 +1,10 @@
 package com.example.liststart.datasource
 
-<<<<<<< HEAD
 import com.example.liststart.model.AndroidDispatchDTO
 import retrofit2.Response
 
 interface DispatchDataSource {
     suspend fun getDispatchList(warehouseNo: Int): Response<List<AndroidDispatchDTO>>
 //    suspend fun updateDispatch(bno: Long, dispatch: Dispatch): Response<Dispatch>
-=======
-import com.example.liststart.model.Dispatch
-import retrofit2.Response
 
-interface DispatchDataSource {
-    suspend fun getDispatchList(): Response<List<Dispatch>>
-    suspend fun updateDispatch(bno: Long, dispatch: Dispatch): Response<Dispatch>
->>>>>>> 3443f5f092f6b9bfd56a6e67cc8f12c502601a8f
 }
--- liststart/src/main/java/com/example/liststart/view/WarehouseList.kt
+++ liststart/src/main/java/com/example/liststart/view/WarehouseList.kt
@@ -12,16 +12,12 @@ class WarehouseList : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_warehouse_list)
 
-<<<<<<< HEAD
-=======
-        // 창고 버튼 TextView 찾기
->>>>>>> 3443f5f092f6b9bfd56a6e67cc8f12c502601a8f
         val headOfficeButton = findViewById<TextView>(R.id.headOfficeWarehouseTextView)
         val incheonButton = findViewById<TextView>(R.id.incheonWarehouseButton)
         val busanButton = findViewById<TextView>(R.id.busanWarehouseButton)
         val cheonanButton = findViewById<TextView>(R.id.cheonanWarehouseButton)
 
-<<<<<<< HEAD
+
         // 창고 선택 시 창고 번호 전달
         headOfficeButton.setOnClickListener { openDeliveryActivity(3) }
         incheonButton.setOnClickListener { openDeliveryActivity(2) }
@@ -32,32 +28,6 @@ class WarehouseList : AppCompatActivity() {
     private fun openDeliveryActivity(warehouseNo: Int) {
         val intent = Intent(this, Delivery::class.java)
         intent.putExtra("warehouse_no", warehouseNo)
-=======
-        // 클릭 리스너 설정하여 DeliveryActivity로 이동하면서 창고 이름 전달
-        headOfficeButton.setOnClickListener {
-            openDeliveryActivity("본사 창고")
-        }
-
-        incheonButton.setOnClickListener {
-            openDeliveryActivity("인천 창고")
-        }
-
-        busanButton.setOnClickListener {
-            openDeliveryActivity("부산 창고")
-        }
-
-        cheonanButton.setOnClickListener {
-            openDeliveryActivity("천안 창고")
-        }
-    }
-
-    private fun openDeliveryActivity(warehouseName: String) {
-        // DeliveryActivity로 Intent 생성
-        val intent = Intent(this, Delivery::class.java)
-        // 창고 이름 데이터를 Intent에 추가
-        intent.putExtra("warehouse_name", warehouseName)
-        // DeliveryActivity 시작
->>>>>>> 3443f5f092f6b9bfd56a6e67cc8f12c502601a8f
         startActivity(intent)
     }
 }
